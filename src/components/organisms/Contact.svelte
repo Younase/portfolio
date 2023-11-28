5cb48692415374405af08227621235c3e6555e5d
Younase <youigta@gmail.com>
2023-11-28 02:44:02 +0100

    feat: add contact section

--- src/components/organisms/Contact.svelte
+++ src/components/organisms/Contact.svelte
@@ -1,7 +1,58 @@
 <script>
+    import 'iconify-icon'
 </script>
 
 <section class="section" id="contact">
+    <div class="container is-max-desktop">
+        <div class="columns">
+            <div class="column is-6">
+                <div class="block">
+                    <h1 class="title is-1">
+                        Get In Touch
+                    </h1>
+                </div>
+                <div class="has-text-centered is-size-3">
+                    <a href="mailto:email@example.com">
+                        email@example.com
+                    </a>
+                </div>
+            </div>
+            <div class="column is-2">
+            </div>
+            <div class="column is-3">
+                <div class="block">
+                    <h1 class="title is-3">
+                        Socials
+                    </h1>
+                </div>
+                <div class="buttons">
+                            <a class="button is-flex is-justify-content-left my-1 is-fullwidth" href="mailto:email@example.com">
+                                <span class="icon px-3">
+                                    <iconify-icon icon="line-md:email" height="auto"></iconify-icon>
+                                </span>
+                                <span>
+                                    email@example.com
+                                </span>
+                            </a>
+                            <a class="button is-normal is-flex is-justify-content-left my-1 is-fullwidth" href="https://www.github.com/younase">
+                                <span class="icon px-3">
+                                    <iconify-icon icon="line-md:github" height="auto"></iconify-icon>
+                                </span>
+                                <span>
+                                    github.com/younase
+                                </span>
+                            </a>
+                    <a class="button is-flex is-justify-content-left my-1 is-fullwidth" href="https://www.linkedin.com">
+                        <span class="icon pr-3 ml-0">
+                            <iconify-icon icon="line-md:linkedin" height="auto"></iconify-icon>
+                        </span>
+                        <span>
+                            linkedin.com
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
 </section>
 
 
